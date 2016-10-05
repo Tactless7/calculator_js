1ddbb00bff787606db853452a47587de6a3bc1c5
Juliane Blier <blier.juliane@gmail.com>
2016-10-05 14:24:19 +0200

    Finalisation de la calculatrice

--- app.js
+++ app.js
@@ -29,26 +29,32 @@ function divide(a,b){
 }
 
 function calc(x,y,z){
-	if (z === 'addition'){
+	if (z === '+'){
 		var result = add(x,y);
 		return result;
 	}
-	else if (z === 'substraction'){
+	else if (z === '-'){
 		var result = substract(x,y);
 		return result;
 	}
-	else if (z === 'multiplication'){
+	else if (z === 'x'){
 		var result = multiply(x,y);
 		return result;
 
 	}
-	else if (z === 'division'){
+	else if (z === '/'){
 		var result = divide(x,y);
 		return result;
 	}
 	else {
 		return alert('Erreur totalement anormale ! Mais que s\'est-il passé ?');
 	}
+}
+
+function resultEqual(){
+	resultat = calc(nb1,nb2,ope);
+	console.log(resultat);
+	$('#result').text(resultat);
 }
 
 		//Lorsqu'on clique sur un bouton numérique
@@ -59,66 +65,38 @@ $('.number').click(function(){
 
 	if(nb1 === null) {
 		nb1 = numButton;
-		$('#result').append(numButton)
+		$('#result').append(numButton);
 	}
 	else if (nb2 === null) {
 		nb2 = numButton;
-		$('#result').append(numButton)
+		$('#result').append(numButton);
 	}
 	else {
 		alert('Remettez à 0 avec CE');
 	}
-
+	$('.number').blur();
 });
-
-
-
+		
 		// Lorsqu'on clique sur un opérateur
 
-$('#btn-plus').click(function(){
+$('.ope').click(function(){
+	var operateur = $(this).data('ope');
+	console.log(operateur);
 	if(ope === null){
-		ope = 'addition';
-		$('#result').append(' + ');
+		ope = operateur;
+		$('#result').append(' ' + operateur + ' ');
 	}
 	else {
 		alert('Vous avez déjà choisi un opérateur, remettez à 0 avec CE si besoin.')
 	}
-});
-$('#btn-minus').click(function(){
-	if(ope === null){
-		ope = 'substraction';
-		$('#result').append(' - ');
-	}
-	else {
-		alert('Vous avez déjà choisi un opérateur, remettez à 0 avec CE si besoin.')
-	}
-});
-$('#btn-div').click(function(){
-	if(ope === null){
-		ope = 'division';
-		$('#result').append(' / ');
-	}
-	else {
-		alert('Vous avez déjà choisi un opérateur, remettez à 0 avec CE si besoin.')
-	}
-});
-$('#btn-mult').click(function(){
-	if(ope === null){
-		ope = 'multiplication';
-		$('#result').append(' x ');
-	}
-	else {
-		alert('Vous avez déjà choisi un opérateur, remettez à 0 avec CE si besoin.')
-	}
-});
-
-		
+	$('.ope').blur();
+});	
+	
 		// Lorsqu'on clique sur égal
 
 $('#btn-equal').click(function(){
-	resultat = calc(nb1,nb2,ope);
-	console.log(resultat);
-	$('#result').text(resultat);
+	resultEqual();
+	$('#btn-equal').blur();
 });
 
 
@@ -130,5 +108,183 @@ $('#btn-del').click(function(){
 	ope = null;
 	$('#result').text('');
 	console.log('Remise à zéro');
+	$('#btn-del').blur();
 });
 
+
+		// Input clavier
+
+$(document).keypress(function(e){
+	if (e.which === 13 || e.which === 61) {
+		console.log('keypress 13')
+		resultEqual(); 
+		//alert('you pressed enter !');		// CA MAAAAAARCHE	
+	}
+	else if (e.which === 48){
+		if (nb1 === null) {
+			nb1 = 0 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 0 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 49){
+		if (nb1 === null) {
+			nb1 = 1 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 1 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 50){
+		if (nb1 === null) {
+			nb1 = 2 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 2 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 51){
+		if (nb1 === null) {
+			nb1 = 3 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 3 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 52){
+		if (nb1 === null) {
+			nb1 = 4 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 4 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 53){
+		if (nb1 === null) {
+			nb1 = 5 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 5 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 54){
+		if (nb1 === null) {
+			nb1 = 6 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 6 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 55){
+		if (nb1 === null) {
+			nb1 = 7 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 7 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 56){
+		if (nb1 === null) {
+			nb1 = 8 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 8 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+	else if (e.which === 57){
+		if (nb1 === null) {
+			nb1 = 9 ;
+			$('#result').append(nb1);
+		}
+		else if (nb2 === null){
+			nb2 = 9 ;
+			$('#result').append(nb2);
+		}
+		else {
+			alert('Remettez à 0 avec CE.');
+		}
+	}
+
+	else if (e.which === 43){
+		if (ope === null){
+			ope = '+' ;
+			$('#result').append(' ' + ope + ' ');
+		}
+		else{
+			alert('Vous avez déjà choisi un opérateur, remettre à 0 avec CE si besoin');
+		}
+	}
+	else if (e.which === 45){
+		if (ope === null){
+			ope = '-' ;
+			$('#result').append(' ' + ope + ' ');
+		}
+		else{
+			alert('Vous avez déjà choisi un opérateur, remettre à 0 avec CE si besoin');
+		}
+	}
+	else if (e.which === 47){
+		if (ope === null){
+			ope = '/' ;
+			$('#result').append(' ' + ope + ' ');
+		}
+		else{
+			alert('Vous avez déjà choisi un opérateur, remettre à 0 avec CE si besoin');
+		}
+	}
+	else if (e.which === 42){
+		if (ope === null){
+			ope = '*' ;
+			$('#result').append(' ' + ope + ' ');
+		}
+		else{
+			alert('Vous avez déjà choisi un opérateur, remettre à 0 avec CE si besoin');
+		}
+	}
+});	
